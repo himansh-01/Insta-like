320f5d140a6d6ed86de18d2d0ab6c4891274854f
himansh-01 <165716300+himansh-01@users.noreply.github.com>
2024-09-11 22:20:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ var love = document.querySelector("i")
 con.addEventListener("dblclick", function(){
     love.style.transform = 'translate(-50%, -50%) scale(1)'
     love.style.opacity = 0.8
-})
 
 setTimeout(function(){
     love.style.opacity = 0
@@ -13,3 +12,4 @@ setTimeout(function(){
 setTimeout(function(){
     love.style.transform = 'translate(-50%, -50%) scale(0)'
 },2000)
+})    
